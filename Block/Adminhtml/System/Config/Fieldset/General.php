18cd69168e97d62e2ec592f1a310a80e373293d8
Renen Donkers <rene@lotsofpixels.io>
2024-02-25 08:54:19 +0000

    Fixed block for getRedisport

--- Block/Adminhtml/System/Config/Fieldset/General.php
+++ Block/Adminhtml/System/Config/Fieldset/General.php
@@ -5,7 +5,10 @@ namespace Lotsofpixels\RedisMonitor\Block\Adminhtml\System\Config\Fieldset;
 
 
 use Magento\Backend\Block\Template;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
+use Magento\Store\Model\ScopeInterface;
 
 
 /**
@@ -13,82 +16,114 @@ use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
  */
 class General extends Template implements RendererInterface
 {
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
     /**
      * Class constructor.
      * @param Template\Context $context
-     * @param \Magento\Framework\Module\ModuleList $moduleList
      * @param array $data
      */
     public function __construct(
-        Template\Context                     $context,
-        \Magento\Framework\Module\ModuleList $moduleList,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        array                                $data = []
+        Template\Context     $context,
+        ScopeConfigInterface $scopeConfig,
+        array                $data = []
     )
     {
         $this->_template = 'Lotsofpixels_RedisMonitor::redis_monitor.phtml';
         $this->scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
-        $this->moduleList = $moduleList;
     }
+
     /**
-     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @param AbstractElement $element
      * @return string
      */
-    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    public function render(AbstractElement $element): string
     {
         $_element = $element;
         return $this->toHtml();
     }
 
     /**
-     * @return void
+     * @return mixed
      */
-    public function getRedisport1() {
-        $valueFromConfig = $this->scopeConfig->getValue(
-            'lotsofpixels_redis/config/redis_port1',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-        );
-}
+    public function getRedisport1(): mixed
+    {
+        return $this->scopeConfig->getValue("lotsofpixels_redis/config/redis_port1", ScopeInterface::SCOPE_STORE);
+    }
 
     /**
-     * @return void
+     * @return mixed
      */
-    public function getRedisport2() {
-        $valueFromConfig = $this->scopeConfig->getValue(
-            'lotsofpixels_redis/config/redis_port2',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-        );
+    public function getRedisport2(): mixed
+    {
+        return $this->scopeConfig->getValue("lotsofpixels_redis/config/redis_port2", ScopeInterface::SCOPE_STORE);
     }
+
     /**
      * @return array
      */
+    public function getRedisCli1(): array
+    {
+        $redisport1 = $this->getRedisport1();
+        $redisoutput1 = [];
+        $clicommmand1 = 'redis-cli -p ' . $redisport1 . ' info memory';
+        $redismemory1 = shell_exec($clicommmand1);
+        if (is_null($redismemory1)) {
+            $redisoutput1 ['redisport'] = $redisport1;
+            $redisoutput1 ['Error'] = 'Could not connect to Redis at ' . $redisport1 . 'Connection refused';
+            return $redisoutput1;
+        } else {
+            $output1 = preg_split('/\s+/', trim($redismemory1));
 
-    public function getRedisCli1() {
-        $redismemory1 = shell_exec('redis-cli info memory');
-        $output1 = preg_split('/\s+/', trim($redismemory1));
-        $redisoutputcli1 = [];
-        foreach ($output1 as $line1) {
-            $splitline1 = '';
-            $kommastring1 = str_replace(':', ',', $line1);
-            $splitline1 = (explode(',',$kommastring1));
-            $array1 = $splitline1;
-            if (!empty($array ['1'])) {$redisoutputcli1 [$array1['0']] = $array1['1'];}
+            foreach ($output1 as $line1) {
+                $kommastring1 = str_replace(':', ',', $line1);
+                $splitline1 = (explode(',', $kommastring1));
+                $array1 = $splitline1;
+                if (!empty($array1 ['1'])) {
+                    $redisoutput1 [$array1['0']] = $array1['1'];
+                }
+            }
+            if ($redisport1) {
+                $redisoutput1 ['redisport'] = $redisport1;
+            }
+            return $redisoutput1;
         }
-        return $redisoutputcli1;
-}
-    public function getRedisCli2() {
-        $redismemory2 = shell_exec('redis-cli info memory');
-        $output2 = preg_split('/\s+/', trim($redismemory2));
-        $redisoutputcli2 = [];
-        foreach ($output2 as $line2) {
-            $splitline2 = '';
-            $kommastring2 = str_replace(':', ',', $line2);
-            $splitline2 = (explode(',',$kommastring2));
-            $array2 = $splitline2;
-            if (!empty($array ['1'])) {$redisoutputcli2 [$array2['0']] = $array2['1'];}
+    }
+
+    /**
+     * @return array
+     */
+    public function getRedisCli2(): array
+    {
+        $redisport2 = $this->getRedisport2();
+        $redisoutput2 = [];
+        $clicommmand2 = 'redis-cli -p ' . $redisport2 . ' info memory';
+        $redismemory2 = shell_exec($clicommmand2);
+        if (is_null($redismemory2)) {
+            $redisoutput2 ['redisport'] = $redisport2;
+            $redisoutput2 ['Error'] = 'Could not connect to Redis at ' . $redisport2 . 'Connection refused';
+            return $redisoutput2;
+        } else {
+            $output2 = preg_split('/\s+/', trim($redismemory2));
+            $redisoutput2 = [];
+            foreach ($output2 as $line2) {
+                $kommastring2 = str_replace(':', ',', $line2);
+                $splitline2 = (explode(',', $kommastring2));
+                $array2 = $splitline2;
+                if (!empty($array2 ['1'])) {
+                    $redisoutput2 [$array2['0']] = $array2['1'];
+                }
+            }
+            if ($redisport2) {
+                $redisoutput2 ['redisport'] = $redisport2;
+            }
+            return $redisoutput2;
         }
-        return $redisoutputcli2;
     }
 }
 
